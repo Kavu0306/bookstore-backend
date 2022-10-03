0aa7866093265a9e1120fdad8f4c648cb4f30e0d
Kavu0306 <kaviyaradha2000@gmail.com>
2022-10-03 21:56:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+
+// Middlewares
+dotenv.config();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(process.env.DB)
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: true,
+    message: "am always present",
+  });
+});
